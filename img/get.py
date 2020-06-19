2388ed91e204f1224c8a96b6f6f05615cde8bbf2
chenjialiang666 <3024182971@qq.com>
2020-06-19 18:27:12 +0800

    Site updated: 2020-06-19 18:26:56

---
+++ img/get.py
@@ -0,0 +1,16 @@
+import os
+pos=0
+last=0
+while True:
+	a=input()
+	for i in range(len(a)):
+		if a[i]=='/':
+			last=i
+	print(last)
+	b=''
+	for i in range(last+1,len(a)):
+		b=b+a[i]
+	print(b)
+	os.system("wget "+a)
+	os.rename(b,str(pos)+'.jpg')
+	pos=pos+1
